b927f6cd5eb5aed424061290d030ca6207201442
Hurr1kaner <viktor.gerasimw@yandex.ru>
2024-01-15 20:55:36 +0700

    first commit

---
+++ src/Service/ArticleService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Psr\Log\LoggerInterface;
+
+
+class ArticleService implements ArticleServiceInterface
+{
+
+
+
+    public function __construct(
+        private readonly ArticleRepository $articleRepository,
+        private readonly LoggerInterface $logger)
+    {
+    }
+
+    public function getRecentArticles(int $count, ?string $search = null): \Doctrine\ORM\QueryBuilder
+    {
+        $this->logger->info(sprintf('getting %d recent articles', $count));
+        return $this ->articleRepository->getRecentArticles($count, $search);
+    }
+
+    public function getSingleArticleById(int $id): ?Article
+    {
+        return $this->articleRepository->find($id);
+    }
+
+}
